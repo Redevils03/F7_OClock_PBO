1c6ddcbf9742e29d2a97fd18ad8498760beb38d2
Artand <martinchandra99@gmail.com>
2022-07-11 14:41:42 +0700

    upload proyek PBO

---
+++ O'Clock/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace O_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(418, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "00:00:00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tanggal";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader2,
+            this.columnHeader1});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(15, 49);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(462, 354);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Tanggal Alarm";
+            this.columnHeader3.Width = 138;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 2;
+            this.columnHeader2.Text = "Nama Alarm";
+            this.columnHeader2.Width = 179;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.DisplayIndex = 1;
+            this.columnHeader1.Text = "Waktu Alarm";
+            this.columnHeader1.Width = 124;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Tambah";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 24);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Hapus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(238, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 24);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Bagikan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox4.Image = global::O_Clock.Properties.Resources.write;
+            this.pictureBox4.Location = new System.Drawing.Point(311, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(22, 25);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox3.Image = global::O_Clock.Properties.Resources.clock__1_;
+            this.pictureBox3.Location = new System.Drawing.Point(210, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(22, 25);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.Image = global::O_Clock.Properties.Resources.list_remove;
+            this.pictureBox2.Location = new System.Drawing.Point(111, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(22, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(339, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 24);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Kode";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "O\'Clock";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ O'Clock/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Npgsql;
+
+namespace O_Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<string> text = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt")).ToList();
+            foreach (string d in text)
+            {
+                string[] items = d.Split(new char[] { ',' },
+                       StringSplitOptions.RemoveEmptyEntries);
+                ListViewItem item = listView1.Items.Add(items[0].ToString());
+                item.SubItems.Add(items[2].ToString());
+                item.SubItems.Add(items[1].ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToLongDateString();
+            label2.Text = DateTime.Now.ToLongTimeString();
+
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.SubItems[0].Text == label1.Text && item.SubItems[2].Text == label2.Text)
+                {
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "alarm.wav");
+                    player.PlayLooping();
+                    DialogResult result = MessageBox.Show(item.SubItems[1].Text);
+                    if (result == DialogResult.OK || result == DialogResult.Cancel)
+                    {
+                        player.Stop();
+                    }
+                }
+            }
+        }
+
+        // Tombol Hapus
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<string> text = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt")).ToList();
+            List<string> data = new List<string>();
+            foreach (string d in text)
+            {
+                string[] items = d.Split(new char[] { ',' });
+                if (listView1.SelectedItems[0].Text != items[0] && listView1.SelectedItems[0].SubItems[1].Text != items[1] && listView1.SelectedItems[0].SubItems[2].Text != items[2])
+                {
+                    data.Add(items[0] + ',' + items[1] + ',' + items[2] + Environment.NewLine);
+                }
+            }
+            File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt"), String.Empty);
+            foreach (string item in data)
+            {
+                File.AppendAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt"), item);
+            }
+
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView1.SelectedItems[0].Remove();
+            }
+        }
+
+        // Tombol Tambah
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tanggal = Interaction.InputBox("Masukkan Tanggal Alarm Yang Diinginkan", "Tanggal Alarm");
+            string waktu = Interaction.InputBox("Masukkan Waktu Alarm Yang Diinginkan", "Waktu Alarm");
+            string nama = Interaction.InputBox("Masukkan Nama Alarm Yang Diinginkan", "Nama Alarm");
+
+            string data = tanggal + "," + waktu + "," + nama;
+            File.AppendAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt"), data + Environment.NewLine); 
+
+            ListViewItem item = listView1.Items.Add(tanggal);
+            item.SubItems.Add(nama);
+            item.SubItems.Add(waktu);
+        }
+
+        // Tombol Bagikan
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[6];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var kode = new string(stringChars);
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=test;User Id=postgres;Password=qwerty;"))
+                {
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "Insert into alarm (kode) values (@kode)";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@kode", kode));
+                    cmd.ExecuteNonQuery();
+
+                    using (cmd = new NpgsqlCommand("Insert into detail_alarm (kode, tanggal, waktu, nama) values((Select id from alarm where kode = '" + kode + "'), @tanggal, @waktu, @nama)", connection))
+                    {
+                        var tanggal = new NpgsqlParameter("tanggal", DbType.String);
+                        var waktu = new NpgsqlParameter("waktu", DbType.String);
+                        var nama = new NpgsqlParameter("nama", DbType.String);
+                        cmd.Parameters.Add(tanggal);
+                        cmd.Parameters.Add(waktu);
+                        cmd.Parameters.Add(nama);
+                        cmd.Prepare();   
+                        List<string> text = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt")).ToList();
+                        foreach (string d in text)
+                        {
+                            string[] items = d.Split(new char[] { ',' });
+                            tanggal.Value = items[0].ToString();
+                            waktu.Value = items[1].ToString();
+                            nama.Value = items[2].ToString();
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    cmd.Dispose();
+                    connection.Close();
+                    MessageBox.Show("Kode untuk dibagikan : " + kode);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Tombol Masukkan Kode
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string kode = Interaction.InputBox("Masukkan Kode Yang Didapatkan", "Kode Alarm");
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=test;User Id=postgres;Password=qwerty;"))
+                {
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    using (cmd = new NpgsqlCommand("Select * from detail_alarm where kode = (Select id from alarm where kode = '" + kode + "')", connection))
+                    {
+                        string tanggal;
+                        string waktu;
+                        string nama;
+                        NpgsqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            tanggal = reader[2].ToString();
+                            waktu = reader[3].ToString();
+                            nama = reader[4].ToString();
+
+                            string data = tanggal + "," + waktu + "," + nama;
+                            File.AppendAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.txt"), data + Environment.NewLine);
+
+                            ListViewItem item = listView1.Items.Add(tanggal);
+                            item.SubItems.Add(nama);
+                            item.SubItems.Add(waktu);
+                        }
+                    }
+                    cmd.Dispose();
+                    connection.Close();
+                    MessageBox.Show("Jadwal Baru Berhasil Ditambahkan!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
